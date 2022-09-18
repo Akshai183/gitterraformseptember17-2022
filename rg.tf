d0c18c2a697091e9cf440bc9b4ce92bd31b7aa27
Akshai <akshay96036@hotmail.com>
2022-09-18 20:03:57 +0530

    Revert "variables are added"
    This reverts commit adf6658fb6df745930499b551ff1ee343286964c.

--- rg.tf
+++ rg.tf
@@ -1,5 +1,5 @@
 # Create a resource group
 resource "azurerm_resource_group" "pranayaakshai" {
-  name     = var.rg_name
-  location = var.location
+  name     = "pranayaakshai"
+  location = "eastus"
 }
