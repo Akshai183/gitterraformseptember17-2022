fa208daa3b445d1eac51aa0dcc1d902d00a6aaa7
Akshai <akshay96036@hotmail.com>
2022-09-22 15:55:38 +0530

    changes done

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,6 @@
 resource "azurerm_virtual_network" "vpc" {
 <<<<<<< HEAD
+<<<<<<< HEAD
   name                = var.vpc_name
   location            = var.location
   resource_group_name = var.rg_name
@@ -12,18 +13,26 @@ resource "azurerm_virtual_network" "vpc" {
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["8.8.8.8", "8.8.4.4"]
 >>>>>>> parent of 7e19496 (adding storage account)
+=======
+  name                = "vpc"
+  location            = azurerm_resource_group.pranayaakshai.location
+  resource_group_name = azurerm_resource_group.pranayaakshai.name
+  address_space       = ["10.0.0.0/16", "10.28.0.0/16"]
+  dns_servers         = ["8.8.8.8", "8.8.4.4"]
+>>>>>>> parent of 0d9b381 (Revert "Revert "variables are added"")
   tags = {
-    environment = var.environment
+    environment = "Development"
   }
 <<<<<<< HEAD
 }
 resource "azurerm_subnet" "subnet1" {
-  name                 = var.subnet1_name
-  resource_group_name  = var.rg_name
-  virtual_network_name = var.vpc_name
-  address_prefixes     = [var.subnet1_cidr]
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.pranayaakshai.name
+  virtual_network_name = azurerm_virtual_network.vpc.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 resource "azurerm_subnet" "subnet2" {
+<<<<<<< HEAD
   name                 = var.subnet2_name
   resource_group_name  = var.rg_name
   virtual_network_name = var.vpc_name
@@ -31,3 +40,28 @@ resource "azurerm_subnet" "subnet2" {
 =======
 >>>>>>> parent of 7e19496 (adding storage account)
 }
+=======
+  name                 = "subnet2"
+  resource_group_name  = azurerm_resource_group.pranayaakshai.name
+  virtual_network_name = azurerm_virtual_network.vpc.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+resource "azurerm_subnet" "subnet3" {
+  name                 = "subnet3"
+  resource_group_name  = azurerm_resource_group.pranayaakshai.name
+  virtual_network_name = azurerm_virtual_network.vpc.name
+  address_prefixes     = ["10.28.1.0/24"]
+}
+resource "azurerm_subnet" "subnet4" {
+  name                 = "subnet4"
+  resource_group_name  = azurerm_resource_group.pranayaakshai.name
+  virtual_network_name = azurerm_virtual_network.vpc.name
+  address_prefixes     = ["10.28.2.0/24"]
+}
+resource "azurerm_subnet" "subnet5" {
+  name                 = "subnet5"
+  resource_group_name  = azurerm_resource_group.pranayaakshai.name
+  virtual_network_name = azurerm_virtual_network.vpc.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
+>>>>>>> parent of 0d9b381 (Revert "Revert "variables are added"")
